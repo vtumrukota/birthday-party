a14e675f515fe93a934d9a244bb294cd4cd8ec4a
Vivek Tumrukota <vivek@Vivek-Anjali.local>
2023-09-30 23:51:59 -0700

    Use pure CSS for hover info

--- app/components/birthday/BirthdayRow.tsx
+++ app/components/birthday/BirthdayRow.tsx
@@ -1,33 +1,27 @@
 import Card from "@mui/material/Card"
-import { Birthday } from "../../models/Birthday"
 import CardContent from "@mui/material/CardContent"
 import { Avatar } from "@mui/material"
-import { useState } from "react"
+import { Birthday } from "../../models/Birthday"
 
-export const BirthdayRow = ({ birthday }: { birthday: Birthday }): JSX.Element => {
-  const [showDetails, setShowDetails] = useState<boolean>(false);
-
-  return (
-    <Card className="flex flex-row mt-5 border-2 border-stone-300 hover:border-green-700"
-      onMouseEnter={() => setShowDetails(true)}
-      onMouseLeave={() => setShowDetails(false)}>
-      {showDetails ? (
-        <CardContent className="flex flex-col items-center text-sm max-h-32 overflow-y-auto">
-          {birthday.additionalDetails}
-        </CardContent>
-      ) : (
-        <CardContent className="flex flex-col">
-          <div className="flex flex-row items-center"> 
-            <Avatar src={birthday.thumbnail} alt={birthday.name} className="flex-row"/>
-            <h3 className="text-lg ml-2">{birthday.name}</h3>
-          </div>
-          <p className="text-sm pt-2 italic">{birthday.description}</p>
-          <p className="text-sm pt-2">
-            <span>👼 {birthday.birthYear}</span>
-            {birthday.deathYear && <span> - 🪦 {birthday.deathYear}</span>}
-          </p>
-        </CardContent>
-      )}
+export const BirthdayRow = ({ birthday }: { birthday: Birthday }): JSX.Element =>
+  <div className="group mt-5 relative">
+    <Card className="border-2 border-stone-500">
+      <CardContent className="flex flex-col">
+        <div className="flex flex-row items-center"> 
+          <Avatar src={birthday.thumbnail} alt={birthday.name} className="flex-row"/>
+          <h3 className="text-lg ml-2">{birthday.name}</h3>
+        </div>
+        <p className="text-sm pt-2 italic">{birthday.description}</p>
+        <p className="text-sm pt-2">
+          <span>👼 {birthday.birthYear}</span>
+          {birthday.deathYear && <span> - 🪦 {birthday.deathYear}</span>}
+        </p>
+      </CardContent>
     </Card>
-  )
-}
+    {/* show more details about the person on hover */}
+    <Card className="hidden absolute top-0 left-0 right-0 bottom-0 bg-white transition duration-300 group-hover:block">
+      <CardContent className="flex flex-col items-center text-sm max-h-32 overflow-y-auto italic">
+        {birthday.additionalDetails}
+      </CardContent>
+    </Card>
+  </div>
